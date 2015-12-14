6064264684124924cef8bb8cecc4e96ae10b40b3
Vasco Mouta <vasco.mouta@gmail.com>
2015-12-14 11:42:38 +0100

    Initial commit

---
+++ Example/VMLogger/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VMLogger
+//
+//  Created by Vasco Mouta on 12/14/2015.
+//  Copyright (c) 2015 Vasco Mouta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
